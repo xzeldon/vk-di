94ed8d3d87342b49227c0315f0b1dc230d744d9b
xzeldon <contact@zeldon.ru>
2021-09-05 02:33:44 +0300

    feat: create logger service

--- packages/core/src/common/types/index.ts
+++ packages/core/src/common/types/index.ts
@@ -1,3 +1,11 @@
 export interface ICommandOptions {
     prefix?: string;
 }
+
+export interface ILogger {
+    log(message: any, context?: string): void;
+    error(message: any, trace?: string, context?: string): void;
+    warn(message: any, context?: string): void;
+    debug?(message: any, context?: string): void;
+    verbose?(message: any, context?: string): void;
+}
