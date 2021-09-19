4c06d24fdc74327bf1d11562000f377fc5053210
xzeldon <contact@zeldon.ru>
2021-09-20 02:42:46 +0300

    feat: :art: more examples

--- packages/simple-bot/src/main.module.ts
+++ packages/simple-bot/src/main.module.ts
@@ -1,4 +1,5 @@
-import { Client, Module } from "@core/*";
+import { Client, Module, On } from "@core/*";
+import { MessageContext } from "vk-io";
 
 export default class MainModule extends Module {
     constructor() {
@@ -7,5 +8,25 @@ export default class MainModule extends Module {
         }));
 
         this.client.start();
+    }
+
+    /**
+     * 
+     * On any message recived log it to stdout
+     */
+    @On("message_new")
+    on_message(context: MessageContext) {
+        if (context.text) {
+            console.log(`[${new Date().toLocaleString('ru')}, @id${context.senderId}] ${context.text}`);
+        }
+    }
+
+    /**
+     * 
+     * On typing
+     */
+    @On('typing')
+    on_typing() {
+        console.log('The user types a message');
     }
 }
