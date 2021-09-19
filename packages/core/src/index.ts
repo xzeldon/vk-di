9c2430c79920b1cc0d9e1142498db52e4f81f28e
xzeldon <contact@zeldon.ru>
2021-09-19 18:43:53 +0300

    refactor: :art: add convenient exports, remove deprecated declarations

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,1 +1,7 @@
-import 'reflect-metadata'
+import 'reflect-metadata';
+
+export { Module } from './module/Module';
+export { Client } from './client/Client';
+export { App } from './app/App';
+
+export { Command, Inject, On } from './decorators';
