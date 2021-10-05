e9cc8eed7acf216b79eefadf1a53bd2ae39ea0e4
xzeldon <contact@zeldon.ru>
2021-10-05 08:58:24 +0300

    build: make rollup watch mode work

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,10 @@
 import run from '@rollup/plugin-run';
 import esbuild from 'rollup-plugin-esbuild';
+import alias from "@rollup/plugin-alias";
+import resolve from "rollup-plugin-node-resolve";
 
 import { builtinModules } from 'module';
-import { join as pathJoin } from 'path';
+import path, { join as pathJoin } from 'path';
 
 const MODULES = [
     'core',
@@ -15,6 +17,7 @@ const core_modules = builtinModules.filter(name => (
 
 const dev = process.env.ROLLUP_WATCH === 'true';
 
+const project_root_dir = path.resolve(__dirname);
 const get_module_path = path => (
     pathJoin(__dirname, 'packages', path)
 );
@@ -29,7 +32,12 @@ export default async () => (
             input: pathJoin(src, 'index.ts'),
             output: dist,
             plugins: [
-                esbuild({ experimentalBundling: true }),
+                alias({
+                    entries: [
+                        { find: '@core/*', replacement: "./packages/core/src/index.ts" }
+                    ]
+                }),
+                esbuild(),
                 dev && run()
             ],
             external: [
